3e01d3554936b8f2290a7c7c9326c2b87ef95173
Nada Zirari <zirarinada20@gmail.com>
2025-12-22 17:06:36 +0100

    ajouter le dto concernant lhistorique de vente

---
+++ src/main/java/org/backend/gpds/main/dto/HistoriqueVenteDTO.java
@@ -0,0 +1,11 @@
+package org.backend.gpds.main.dto;
+
+import java.time.LocalDate;
+
+public class HistoriqueVenteDTO {
+    private String produitNom;
+    private String entrepotNom;
+    private LocalDate dateVente;
+    private Integer quantiteVendue;
+
+}

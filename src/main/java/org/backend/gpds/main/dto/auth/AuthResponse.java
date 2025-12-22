79111d3efe6c12d2febf36e8c909ac7e948c2465
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-22 17:18:57 +0100

    Feat : add AuthResponse DTO for returning authentication response data

---
+++ src/main/java/org/backend/gpds/main/dto/auth/AuthResponse.java
@@ -0,0 +1,14 @@
+package org.backend.gpds.main.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String email;
+    private String name;
+    private String id;
+    private String role;
+}

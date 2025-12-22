d638f1c73dd87dbf0674b4fa0e754d9ab9981a67
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-22 15:54:01 +0100

    Initialize Spring Boot project with Maven wrapper, PostgreSQL setup, and basic configuration files

---
+++ src/main/java/org/backend/gpds/GpdsApplication.java
@@ -0,0 +1,13 @@
+package org.backend.gpds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpdsApplication.class, args);
+    }
+
+}

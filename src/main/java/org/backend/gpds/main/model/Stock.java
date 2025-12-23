52dca0ecfa791968b286bd0af3f524260515bc72
Nada Zirari <zirarinada20@gmail.com>
2025-12-23 17:06:35 +0100

    ajouter entite stock

--- src/main/java/org/backend/gpds/main/model/Stock.java
+++ src/main/java/org/backend/gpds/main/model/Stock.java
@@ -1,4 +1,26 @@
 package org.backend.gpds.main.model;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
 public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Produit produit;
+
+    @ManyToOne
+    private Entrepot entrepot;
+
+    private int quantiteDisponible;
+
+    private int seuilAlerte;
 }

afb99de2459906004ecefc3054d5f305fe95a9f8
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-23 10:14:07 +0100

    Feat : implement AdminseederRunner to seed default admin user at startup

--- src/main/java/org/backend/gpds/main/seeder/AdminseederRunner.java
+++ src/main/java/org/backend/gpds/main/seeder/AdminseederRunner.java
@@ -1,4 +1,43 @@
 package org.backend.gpds.main.seeder;
 
-public class AdminseederRunner {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.backend.gpds.Enums.Role;
+import org.backend.gpds.main.model.User;
+import org.backend.gpds.main.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class AdminseederRunner  implements CommandLineRunner {
+
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        seedUsers();
+    }
+
+    private void seedUsers() {
+        if (!userRepository.existsByEmail("admin@gpds.com")) {
+            User admin = new User(
+                    1L,
+                    "Admin",
+                    "admin@gpds.com",
+                    passwordEncoder.encode("admin123"),
+                    Role.ADMIN
+            );
+            userRepository.save(admin);
+            log.info(" Admin created: admin@gpds.com / admin123");
+        }
+    }
 }

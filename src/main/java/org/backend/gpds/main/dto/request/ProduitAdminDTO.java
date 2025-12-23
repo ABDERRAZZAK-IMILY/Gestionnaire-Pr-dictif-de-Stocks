9c09711535c21e5edfa0e6337e04c1e186c5eb87
Nada Zirari <zirarinada20@gmail.com>
2025-12-23 17:12:20 +0100

    ajouter dtoproduuitadmin

---
+++ src/main/java/org/backend/gpds/main/dto/request/ProduitAdminDTO.java
@@ -0,0 +1,18 @@
+package org.backend.gpds.main.dto.request;
+
+
+import org.backend.gpds.Enums.Unite;
+import lombok.*;
+
+@Getter @Setter
+public class ProduitAdminDTO {
+    private Long id;
+    private String nom;
+    private String description;
+    private String categorie;
+    private double prixVente;
+    private double prixAchat;
+    private double marge;
+    private double poids;
+    private Unite unite;
+}

27b737f0473d8f2a926a9029ec2631b149ea5743
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-23 14:56:22 +0100

    Chore : add Profile annotation import to UserRepository for profile-based activation

--- src/main/java/org/backend/gpds/main/repository/jpa/UserRepository.java
+++ src/main/java/org/backend/gpds/main/repository/jpa/UserRepository.java
@@ -1,12 +1,14 @@
 package org.backend.gpds.main.repository.jpa;
 
 import org.backend.gpds.main.model.User;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+//@Profile("postgres")
 public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByEmail(String email);

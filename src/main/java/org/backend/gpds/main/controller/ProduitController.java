32f4fe2c712f6e10144d9663e1147e929cdd8cf2
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-24 11:07:47 +0100

    refactor: update ProduitController to use explicit constructor injection for ProduitServiceImpl

--- src/main/java/org/backend/gpds/main/controller/ProduitController.java
+++ src/main/java/org/backend/gpds/main/controller/ProduitController.java
@@ -6,9 +6,10 @@ import org.backend.gpds.main.dto.request.ProduitAdminDTO;
 import org.backend.gpds.main.dto.request.ProduitCreateDTO;
 import org.backend.gpds.main.dto.request.ProduitUpdateDTO;
 import org.backend.gpds.main.mapper.ProduitMapper;
-import org.backend.gpds.main.service.impl.ProduitServiceImpl;
+import org.backend.gpds.main.service.ProduitService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+import org.backend.gpds.main.service.impl.ProduitServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -17,10 +18,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/produits")
-@RequiredArgsConstructor
 public class ProduitController {
 
-    private final ProduitService produitService;
+    private final ProduitServiceImpl produitService;
+
+
+    @Autowired
+    public ProduitController(ProduitServiceImpl produitService) {
+        this.produitService = produitService;
+    }
 
     @GetMapping
     public List<?> getAll(Authentication authentication) {

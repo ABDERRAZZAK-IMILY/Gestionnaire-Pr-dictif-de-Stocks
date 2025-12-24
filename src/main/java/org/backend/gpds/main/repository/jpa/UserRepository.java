e68e06f510c4bea2e4cffe5617bd6bc98cb8a878
Nada Zirari <zirarinada20@gmail.com>
2025-12-24 16:37:57 +0100

    ajouter interface user service

--- src/main/java/org/backend/gpds/main/repository/jpa/UserRepository.java
+++ src/main/java/org/backend/gpds/main/repository/jpa/UserRepository.java
@@ -1,40 +1,13 @@
 package org.backend.gpds.main.repository.jpa;
 
 import org.backend.gpds.main.model.User;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import  org.backend.gpds.Enums.Role;
-
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    /**
-     * Utilisé pour l'authentification JWT
-     */
     Optional<User> findByLogin(String login);
 
-    /**
-     * Vérifier unicité du login
-     */
-    boolean existsByLogin(String login);
-
-    /**
-     * Vérifier unicité de l'email
-     */
-    boolean existsByEmail(String email);
-
-    /**
-     * ADMIN : voir tous les gestionnaires
-     */
-    List<User> findByRole(Role role);
-
-    /**
-     * ADMIN : voir les gestionnaires par entrepôt
-     */
-    List<User> findByRoleAndEntrepotId(Role role, Long entrepotId);
 }
--- src/main/java/org/backend/gpds/main/service/UserService.java
+++ src/main/java/org/backend/gpds/main/service/UserService.java
@@ -6,48 +6,17 @@ import org.backend.gpds.main.dto.request.UserUpdateDTO;
 import java.util.List;
 
 public interface UserService {
-
-    /**
-     * ADMIN uniquement
-     * Créer un utilisateur
-     * - ADMIN : entrepot = null
-     * - GESTIONNAIRE : entrepot obligatoire
-     */
     UserResponseDTO createUser(UserCreateDTO dto);
 
-    /**
-     * ADMIN uniquement
-     * Modifier un utilisateur
-     */
     UserResponseDTO updateUser(Long userId, UserUpdateDTO dto);
 
-    /**
-     * ADMIN uniquement
-     * Activer / désactiver un utilisateur
-     */
-    void toggleUserActivation(Long userId, boolean active);
+    void toggleUserActivation(Long userId, boolean actif);
 
-    /**
-     * ADMIN uniquement
-     * Supprimer un utilisateur
-     */
     void deleteUser(Long userId);
 
-    /**
-     * ADMIN uniquement
-     * Liste de tous les utilisateurs
-     */
     List<UserResponseDTO> getAllUsers();
 
-    /**
-     * ADMIN uniquement
-     * Détails d’un utilisateur
-     */
     UserResponseDTO getUserById(Long userId);
 
-    /**
-     * Utilisateur connecté
-     * Récupérer son propre profil
-     */
     UserResponseDTO getCurrentUserProfile(String login);
 }

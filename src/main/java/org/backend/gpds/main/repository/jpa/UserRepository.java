8e2e70f5d7cda789f620eb53f94a95ee0b076c12
Nada Zirari <zirarinada20@gmail.com>
2025-12-24 16:14:00 +0100

    ajouter repository de user

--- src/main/java/org/backend/gpds/main/repository/jpa/UserRepository.java
+++ src/main/java/org/backend/gpds/main/repository/jpa/UserRepository.java
@@ -4,14 +4,37 @@ import org.backend.gpds.main.model.User;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import  org.backend.gpds.Enums.Role;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-//@Profile("postgres")
-public interface UserRepository extends JpaRepository<User, String> {
 
-    Optional<User> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    boolean existsByEmail(String login);
+    /**
+     * Utilisé pour l'authentification JWT
+     */
+    Optional<User> findByLogin(String login);
+
+    /**
+     * Vérifier unicité du login
+     */
+    boolean existsByLogin(String login);
+
+    /**
+     * Vérifier unicité de l'email
+     */
+    boolean existsByEmail(String email);
+
+    /**
+     * ADMIN : voir tous les gestionnaires
+     */
+    List<User> findByRole(Role role);
+
+    /**
+     * ADMIN : voir les gestionnaires par entrepôt
+     */
+    List<User> findByRoleAndEntrepotId(Role role, Long entrepotId);
 }

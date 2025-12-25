22b581c683131423be7167119497f7037dae2fa4
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-26 00:08:40 +0100

    ajouter l'entité HistoriqueVente avec les champs nécessaires et les annotations MongoDB

--- src/main/java/org/backend/gpds/main/model/HistoriqueVente.java
+++ src/main/java/org/backend/gpds/main/model/HistoriqueVente.java
@@ -1,7 +1,28 @@
 package org.backend.gpds.main.model;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDate;
 
-
+@Document(collection = "historique_ventes")
+@Data @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class HistoriqueVente {
 
+    @Id
+    private String id;
+
+    private Long produitId;
+    private Long entrepotId;
+
+    private String produitNom;
+    private String entrepotNom;
+
+    private int quantiteVendue;
+    private LocalDate dateVente;
+
+    private String jourSemaine;
+    private int mois;
+    private int annee;
 }

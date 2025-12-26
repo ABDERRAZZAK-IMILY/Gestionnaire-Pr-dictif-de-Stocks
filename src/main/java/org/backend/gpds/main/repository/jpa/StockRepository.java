e397f180f77d298e89f47a99796085b9644a89b9
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-26 20:27:06 +0100

    update findQuantityByProductAndEntrepot method in StockRepository to use custom JPQL query

--- src/main/java/org/backend/gpds/main/repository/jpa/StockRepository.java
+++ src/main/java/org/backend/gpds/main/repository/jpa/StockRepository.java
@@ -3,6 +3,8 @@ package org.backend.gpds.main.repository.jpa;
 
 import org.backend.gpds.main.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +13,5 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
 
     List<Stock> findByEntrepotId(Long entrepotId);
 
-    Integer findQuantityByProductAndEntrepot(Long produitId, Long entrepotId);
-}
+    @Query("SELECT s.quantiteDisponible FROM Stock s WHERE s.produit.id = :produitId AND s.entrepot.id = :entrepotId")
+    Integer findQuantityByProductAndEntrepot(@Param("produitId") Long produitId, @Param("entrepotId") Long entrepotId);}

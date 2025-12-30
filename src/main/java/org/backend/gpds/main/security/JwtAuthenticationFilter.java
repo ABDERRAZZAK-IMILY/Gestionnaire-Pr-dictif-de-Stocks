3e7364bbb10b2bc81aa759e0058769f25e1743e1
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-30 12:55:07 +0100

    remove bypass for /iot/ endpoints in JwtAuthenticationFilter

--- src/main/java/org/backend/gpds/main/security/JwtAuthenticationFilter.java
+++ src/main/java/org/backend/gpds/main/security/JwtAuthenticationFilter.java
@@ -35,11 +35,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         String path = request.getRequestURI();
 
-        if (path.startsWith("/iot/")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
 
         String jwt = parseJwt(request);
 
